094bb564ab1958985883e50db6cd6b305d3973da
Caleb Owusu-Yianoma <caleb.owusuyianoma@just-eat.com>
2023-10-30 10:28:19 +0000

    Create executable.

--- src/DiamondKataSolver/Program.cs
+++ src/DiamondKataSolver/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DiamondKataSolver;
 
-Console.WriteLine("Hello, World!");
+const string invalidInputMessage =
+    "Invalid input. Expected usage is `DiamondKataSolver.exe <letter>, where <letter> is a single letter from the English alphabet.";
+
+if (args.Length == 1)
+{
+    Diamond diamond = new();
+    try
+    {
+        string firstInput = args[0];
+        if (firstInput.Length == 1)
+        {
+            char letter = char.Parse(args[0]);
+            Console.WriteLine(diamond.CreateDiamond(letter));
+        }
+        else
+        {
+            Console.WriteLine(invalidInputMessage);
+        }
+    }
+    catch (InvalidOperationException e)
+    {
+        Console.WriteLine($"{invalidInputMessage} Error message: {e}");
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine($"{invalidInputMessage} Error message: {e}");
+    }
+}
+else
+{
+    Console.WriteLine(invalidInputMessage);
+}
