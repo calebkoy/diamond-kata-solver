8b48e2d46b3f7ae457b54a6399b6d61fcfd47508
Caleb Owusu-Yianoma <caleb.owusuyianoma@just-eat.com>
2023-10-30 06:54:04 +0000

    Get tests for correct character sequence passing.

--- src/DiamondKataSolver/Diamond.cs
+++ src/DiamondKataSolver/Diamond.cs
@@ -1,9 +1,25 @@
+using System.Text;
+
 namespace DiamondKataSolver;
 
 internal sealed class Diamond
 {
     internal string CreateDiamond(char girdleLetter)
     {
-        return girdleLetter.ToString();
+        StringBuilder stringBuilder = new StringBuilder();
+        int girdleLetterPosition = GetLetterPosition(girdleLetter);
+        char firstAlphabetLetter = char.IsUpper(girdleLetter) ? 'A' : 'a';
+        for (int i = 0; i < girdleLetterPosition; ++i)
+        {
+            stringBuilder.Append((char)(firstAlphabetLetter + i));
+        }
+
+        return stringBuilder.ToString();
+    }
+    
+    private static int GetLetterPosition(char letter)
+    {
+        char firstAlphabetLetter = char.IsUpper(letter) ? 'A' : 'a';
+        return letter - firstAlphabetLetter + 1;
     }
 }
